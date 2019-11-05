49d418511480a69a036fa4dd6b46aa3605546869
Delfin99 <wjdtmdrl12@gmail.com>
2019-11-05 19:41:32 +0000

    Modified - pyzbar_video.py

--- Codes/pyzbar_video.py
+++ Codes/pyzbar_video.py
@@ -1,41 +1,42 @@
 import pyzbar.pyzbar as pyzbar
-import cv2
+import cv2 
 
 cap = cv2.VideoCapture(0)
 
 while(cap.isOpened()):
-  
-  # Read frame by frame 
-  ret, img = cap.read()
 
-  decoded = pyzbar.decode(img)
+    # Read frame by frame
+    ret, frame = cap.read()
 
-  for d in decoded: 
-    # These are used to store the location of detected barcode or QR code
-    x, y, w, h = d.rect
+    decoded = pyzbar.decode(frame)
 
-    # Based on the above variables, draw the rectangle shape covering the detected barcode or QR code
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    for d in decoded:
 
-    # Get the information and type of the detected barcode or QR code
-    Data = d.data.decode("utf-8")
-    Type = d.type
+        # These are used to store the location of detected barcode or QR code
+        x, y, width, height = d.rect
 
-    print(Data + ' [' + Type + ']')
+        # Based on the above variables, draw the rectangle shape covering the detected barcode or QR code
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
 
-    # Show the above Data and Type information on the top of the rectangle shape
-    text = Data + ' [' + Type + ']'
+        # Get the information and type of the detected barcode or QR code 
+        Data = d.data.decode('utf-8')
+        Type = d.type
 
-    # putText function
-    cv2.putText(img, text, (x, y-10), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        print(Data + ' [' + Type + ']')
 
-  cv2.imshow('img', img)
+        # Show the above Data and Type information on the top of the rectangle shape
+        text = Data + ' [' + Type + ']'
 
-  key = cv2.waitKey(1)
+        # putText function
+        cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
 
-  # Press 'q' to quit
-  if key == ord('q'):
-    break
+    cv2.imshow('Barcode Detection', frame)
+
+    key = cv2.waitKey(1)
+
+    # Press 'q' to quit
+    if key == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
